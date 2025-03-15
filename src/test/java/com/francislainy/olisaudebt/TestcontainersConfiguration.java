45854d279416f9d0f9a308f343a65f06f9446c40
Francislainy <francislainy.campos@gmail.com>
2025-03-15 05:37:19 +0000

    Run the app with test containers

--- src/test/java/com/francislainy/olisaudebt/TestcontainersConfiguration.java
+++ src/test/java/com/francislainy/olisaudebt/TestcontainersConfiguration.java
@@ -3,9 +3,11 @@ package com.francislainy.olisaudebt;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
+@Profile("test")
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
 
